76a9ef5b176f67c8ec95a0b9792f39a823b029c6
César Borges <cesar_silk321@hotmail.com>
2026-01-23 16:39:24 -0300

    Ajustada a condicao de start do docker

--- app/Http/Controllers/RelatoriosController.php
+++ app/Http/Controllers/RelatoriosController.php
@@ -40,6 +40,7 @@ class RelatoriosController extends Controller
         // =========================
         $query = NotaFiscalItem::query()
             ->select([
+                'nota_fiscal_itens.id',
                 'nota_fiscal_itens.nota_fiscal_id',
                 'nota_fiscal_itens.material_id',
                 'nota_fiscal_itens.faturado',
@@ -48,7 +49,7 @@ class RelatoriosController extends Controller
             ->with([
                 'material:codigo,descricao',
                 'notaFiscal:id,empresa_id,cliente_id,nota_fiscal,serie,emissao',
-                'notaFiscal.cliente:id,nome_razaosocial,sobrenome_nomefantasia,cpf_cnpj,telefone,email',
+//                'notaFiscal.cliente:id,nome_razaosocial,sobrenome_nomefantasia,cpf_cnpj,telefone,email',
             ])
             ->whereHas('notaFiscal', function ($q) use ($request) {
 
@@ -124,11 +125,19 @@ class RelatoriosController extends Controller
             ])->render()
         );
 
-        $itens->chunk(200)->each(function ($chunk) use ($totais, $mpdf) {
-            $mpdf->WriteHTML(
-                view('relatorios.table', ['itens' => $chunk, 'totais' => $totais])->render()
-            );
-        });
+        if ($request->cliente_id) {
+            $itens->chunk(200)->each(function ($chunk) use ($totais, $mpdf) {
+                $mpdf->WriteHTML(
+                    view('relatorios.table', ['itens' => $chunk, 'totais' => $totais])->render()
+                );
+            });
+        } else {
+            $itens->chunk(200)->each(function ($chunk) use ($totais, $mpdf) {
+                $mpdf->WriteHTML(
+                    view('relatorios.cliente-table', ['itens' => $chunk, 'totais' => $totais])->render()
+                );
+            });
+        }
 
         $mpdf->WriteHTML(
             view('relatorios.footer', compact('cliente'))->render()
